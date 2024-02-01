6342ccde9061c12fc6155049d3d24dd9a6ebcef7
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-02-02 00:38:40 +0500

    set home page done

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.home.urls')),
+    path('about/', include('apps.about.urls')),
+    path('contact/', include('apps.contact.urls')),
+    path('courses/', include('apps.courses.urls')),
+    path('admission/', include('apps.admission.urls')),
 ]
